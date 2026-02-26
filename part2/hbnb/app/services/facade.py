3f4d3c6089659ee80aff60f115540a63ea966404
LenyBl <blee.leny@gmail.com>
2026-02-26 14:55:12 +0100

    Feat: add reviews namespace to the API and fix import path for Review model

--- part2/hbnb/app/services/facade.py
+++ part2/hbnb/app/services/facade.py
@@ -1,7 +1,7 @@
 from app.persistence.repository import InMemoryRepository
 from app.models.user import User
 from app.models.amenity import Amenity
-from part2.hbnb.app.models.review import Review
+from app.models.review import Review
 from app.models.place import Place
 
 
