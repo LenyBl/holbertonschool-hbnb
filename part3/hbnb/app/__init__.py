4741bb6a1b8725e183d5e207ca9bcb69c0517412
JAuxace <auxance.jourdan@proton.me>
2026-03-12 13:39:50 +0100

    Feat: implement SQLAlchemy repository pattern and update app initialization

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -1,26 +1,25 @@
 from flask import Flask, redirect
 from flask_restx import Api
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
+from flask_sqlalchemy import SQLAlchemy
 from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as reviews_ns
 from app.api.v1.auth import api as auth_ns
 from app.services import facade
-from app.persistence.repository import InMemoryRepository
-from flask_bcrypt import Bcrypt
-from flask_jwt_extended import JWTManager
 
 bcrypt = Bcrypt()
 jwt = JWTManager()
+db = SQLAlchemy()
 
 def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
     app.config.from_object(config_class)
     
-    facade.user_repo = InMemoryRepository()
-    facade.place_repo = InMemoryRepository()
-    facade.review_repo = InMemoryRepository()
-    facade.amenity_repo = InMemoryRepository()
+    db.init_app(app)
+    
     bcrypt.init_app(app)
     jwt.init_app(app)
 
